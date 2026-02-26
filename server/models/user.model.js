04d90d40a2ce97452fc6476725780105ee937efd
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2026-02-26 17:24:55 +0530

    feat(auth): implement user model with email validation, password security and JWT authentication
    - Added mongoose user schema
    - Email normalization and validation
    - Password field security with select:false
    - Static password hashing utility
    - Password comparison method
    - JWT generation method
    - Unique email indexing
    - Enabled timestamps

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    minLength: [6, "Username must be at least 6 characters long"],
+    maxLength: [50, "Username must be at most 50 characters long"],
+  },
+  password: {
+    type: String,
+    select: false,
+    required:true,
+    minLength: [8, "Password must be at least 8 characters long"],
+  },
+},{
+    timestamps: true
+});
+
+userSchema.index({email: 1}, {unique: true});
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.isPasswordValid = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      email: this.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+};
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
